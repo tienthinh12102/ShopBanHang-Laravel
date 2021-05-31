e5123269203bbc57c40102973b8537026a212e8e
Thinh <tienthinh12102@gmail.com>
2021-05-31 11:17:22 +0700

    Add new files laravel

---
+++ resources/views/admin/dashboard_layout.blade.php
@@ -0,0 +1,6 @@
+@extends('admin_layout')
+@section('admin_content')
+
+<h3>Chào mừng bạn đến với Admin 1</h3>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//fontend
+Route::get('/','HomeController@index');
+
+Route::get('/trang-chu','HomeController@index');
+
+
+
+
+//backend
+Route::get('/admin','AdminController@index');
+
+Route::get('/dashboard','AdminController@show_dashboard');
+
+Route::post('/admin-dashboard','AdminController@dashboard');
+Route::get('/logout','AdminController@logout');
+
+
