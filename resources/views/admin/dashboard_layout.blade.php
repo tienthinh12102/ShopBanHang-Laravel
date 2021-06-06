1697bdda986fcea21f14aa8d9605348089ab8a4b
Thinh Nguyen <tienthinh12102>
2021-06-06 17:39:42 +0700

    F2

--- resources/views/admin/dashboard_layout.blade.php
+++ resources/views/admin/dashboard_layout.blade.php
@@ -2,5 +2,10 @@
 @section('admin_content')
 
 <h3>Chào mừng bạn đến với Admin Thinh-sama</h3>
-
+<hr >
+<center>
+	<iframe width="1100" height="530" src="https://www.youtube.com/embed/bmVKaAV_7-A?autoplay=1&mute=1"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+	</iframe>
+</center>>
+	
 @endsection
